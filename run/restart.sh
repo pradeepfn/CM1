af6154b20c17ecac79df7b5155729dc30dac1758
Pradeep Fernando <pradeepfn@gmail.com>
2015-01-26 18:01:02 -0500

    jedip specific commits

--- run/restart.sh
+++ run/restart.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
+node_x=3
+node_y=3
 
+let no_of_procs=$node_x*$node_y
+restart_freq=1000
+
+echo $node_x
+echo $node_y
 cp namelist.input.orig namelist.input 
-sed -i "s/rstfrq = -3600.0/rstfrq = 10/" namelist.input
+sed -i "s/nodex        =       1/nodex =  $node_x/" namelist.input
+sed -i "s/nodey        =       1/nodey =  $node_y/" namelist.input
+sed -i "s/rstfrq = -3600.0/rstfrq = $restart_freq/" namelist.input
 sed -i "s/irst      =  0/irst      =  1/" namelist.input
 sed -i "s/rstnum    =  1/irst      =  3/" namelist.input
-mpirun -n 4 ./cm1.exe
+mpirun -np $no_of_procs ./cm1.exe
--- run/run.sh
+++ run/run.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
+
+node_x=3
+node_y=3
+
+let no_of_procs=$node_x*$node_y
+restart_freq=10
+
+echo $node_x
+echo $node_y
+
 make restartclean
 cp namelist.input.orig namelist.input 
-sed -i "s/rstfrq = -3600.0/rstfrq = 10/" namelist.input
-mpirun -np 4 ./cm1.exe
+sed -i "s/nodex        =       1/nodex =  $node_x/" namelist.input
+sed -i "s/nodey        =       1/nodey =  $node_y/" namelist.input
+sed -i "s/rstfrq = -3600.0/rstfrq = $restart_freq/" namelist.input
+mpirun -np $no_of_procs ./cm1.exe
