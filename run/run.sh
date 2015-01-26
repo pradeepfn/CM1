2dc403ea414879e8414d535aacc6614974dbec2d
Pradeep Fernando <pradeepfn@gmail.com>
2015-01-26 18:14:00 -0500

    host file added

--- run/run.sh
+++ run/run.sh
@@ -15,4 +15,4 @@ cp namelist.input.orig namelist.input
 sed -i "s/nodex        =       1/nodex =  $node_x/" namelist.input
 sed -i "s/nodey        =       1/nodey =  $node_y/" namelist.input
 sed -i "s/rstfrq = -3600.0/rstfrq = $restart_freq/" namelist.input
-mpirun -np $no_of_procs ./cm1.exe
+mpirun -np $no_of_procs -f host_file ./cm1.exe
