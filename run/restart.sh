fe24f376a027f3fcb0ff60d4446086c39ce74edb
Pradeep Fernando <pradeepfn@gmail.com>
2015-03-15 12:03:28 -0400

    removing analytics writing in to file

--- run/restart.sh
+++ run/restart.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo 1 >| notify/cm1.notify
+echo 0 >| notify/cm1.notify
 node_x=4
 node_y=4
 
@@ -14,4 +14,6 @@ sed -i "s/nodey        =       1/nodey =  $node_y/" namelist.input
 sed -i "s/rstfrq = -3600.0/rstfrq = $restart_freq/" namelist.input
 sed -i "s/irst      =  0/irst      =  5/" namelist.input
 sed -i "s/rstnum    =  1/rstnum      =  3/" namelist.input
-mpirun -np $no_of_procs -f host_file ./cm1.exe
+#mpirun -np $no_of_procs -f host_file ./cm1.exe
+mpirun -np $no_of_procs ./cm1.exe
+
--- run/run.sh
+++ run/run.sh
@@ -5,7 +5,7 @@ node_x=4
 node_y=4
 
 let no_of_procs=$node_x*$node_y
-restart_freq=120
+restart_freq=20
 
 echo $node_x
 echo $node_y
@@ -15,4 +15,5 @@ cp namelist.input.orig namelist.input
 sed -i "s/nodex        =       1/nodex =  $node_x/" namelist.input
 sed -i "s/nodey        =       1/nodey =  $node_y/" namelist.input
 sed -i "s/rstfrq = -3600.0/rstfrq = $restart_freq/" namelist.input
-mpirun -np $no_of_procs -f host_file ./cm1.exe
+#mpirun -np $no_of_procs -f host_file ./cm1.exe
+mpirun -np $no_of_procs ./cm1.exe
