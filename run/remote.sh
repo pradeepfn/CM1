ee7a9b745825fe02be51ca4c80646d35add7aa34
Pradeep Fernando <pradeepfn@gmail.com>
2015-04-25 19:58:36 -0400

    script updates

--- run/remote.sh
+++ run/remote.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo 0 >| notify/cm1.notify
-
+echo 1 >| notify/cm1.notify
+rm -rf stats/*
 node_x=4
 node_y=4
 
@@ -9,11 +9,13 @@ restart_freq=10
 
 echo $node_x
 echo $node_y
-
-make restartclean
 cp namelist.input.orig namelist.input 
-cp phoenix.config.run phoenix.config
+cp phoenix.config.remote phoenix.config
 sed -i "s/nodex        =       1/nodex =  $node_x/" namelist.input
 sed -i "s/nodey        =       1/nodey =  $node_y/" namelist.input
 sed -i "s/rstfrq = -3600.0/rstfrq = $restart_freq/" namelist.input
-mpirun -np $no_of_procs -f host_file ./cm1.exe
+sed -i "s/irst      =  0/irst      =  5/" namelist.input
+sed -i "s/rstnum    =  1/rstnum      =  3/" namelist.input
+mpirun -np $no_of_procs -f host_file ./cm1.exe >mylog.log 2>&1 & 
+#mpirun -np $no_of_procs ./cm1.exe
+
--- run/restart.sh
+++ run/restart.sh
@@ -10,11 +10,12 @@ restart_freq=10
 echo $node_x
 echo $node_y
 cp namelist.input.orig namelist.input 
+cp phoenix.config.restart phoenix.config
 sed -i "s/nodex        =       1/nodex =  $node_x/" namelist.input
 sed -i "s/nodey        =       1/nodey =  $node_y/" namelist.input
 sed -i "s/rstfrq = -3600.0/rstfrq = $restart_freq/" namelist.input
 sed -i "s/irst      =  0/irst      =  5/" namelist.input
 sed -i "s/rstnum    =  1/rstnum      =  3/" namelist.input
-mpirun -np $no_of_procs -f host_file ./cm1.exe > mylog.log &
+mpirun -np $no_of_procs -f host_file ./cm1.exe > mylog.log  2>&1 &
 #mpirun -np $no_of_procs ./cm1.exe
 
