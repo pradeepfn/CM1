059ddc1d8bd8ce2309752715da3276ac442d7b39
Pradeep Fernando <pradeepfn@gmail.com>
2015-10-13 17:23:35 -0400

    adding timestamp, init calls, modifying the allocate wrapper

--- run/run.sh
+++ run/run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 
-node_x=3
-node_y=3
+node_x=2
+node_y=2
 
 let no_of_procs=$node_x*$node_y
 restart_freq=10
@@ -10,9 +10,9 @@ restart_freq=10
 echo $node_x
 echo $node_y
 
-make restartclean
+make rclean
 cp namelist.input.orig namelist.input 
 sed -i "s/nodex        =       1/nodex =  $node_x/" namelist.input
 sed -i "s/nodey        =       1/nodey =  $node_y/" namelist.input
 sed -i "s/rstfrq = -3600.0/rstfrq = $restart_freq/" namelist.input
-mpirun -np $no_of_procs -f host_file ./cm1.exe
+mpirun -np $no_of_procs ./cm1.exe
